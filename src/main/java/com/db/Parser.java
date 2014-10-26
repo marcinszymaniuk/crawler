2d4ddad08bc1df2fe48f1c3bd1119f323dea92bb
Marcin Szymaniuk <marcin@spotify.com>
2014-10-26 20:43:48 +0100

    Parser refactored to use jsoup Document as input.
    - parse method takes jsoup document as input
    - parse method returns price for now
    - created example unit test

--- src/main/java/com/db/Parser.java
+++ src/main/java/com/db/Parser.java
@@ -18,65 +18,87 @@ import java.util.Locale;
  */
 public class Parser {
 
-    File input = new File("///home/marcin/RAFAŁ/NICE/theColdestPlace/OPEL.html");
-    public void parseFromFile() {
 
-        double price = 0 ;
+
+    public Document getJsoupDocument() {
+
+        File input = new File(getClass().getResource("/kostki.html").getFile());
         String costOfDeliveryString = null;
         try {
-
-            List<Double> listOfCosts = new LinkedList<Double>();
-
-            Document doc = Jsoup.parse(input, "UTF-8","http://allegro.pl/dobre-struny-do-gitary-akustycznej-10-47-3x-kostka-i4639566521.html");
-
-            Element link = doc.select("strong[data-price]").first();
-            Element link1 = doc.select("span[itemprop]").first();
-            Elements link2 = doc.select("strong[class]");
-
-//            System.out.print(link2);
-//            System.out.println(link2.attr("class"));
-
-            for(Element src:link2){
-                if(src.attr("class").equals("whiteBg")){
-                    costOfDeliveryString = src.text().trim().split(" z")[0];
-                    try {
-                        NumberFormat format = NumberFormat.getInstance(Locale.FRANCE);
-                        Number number = format.parse(costOfDeliveryString);
-                        double costOfDelivery = number.doubleValue();
-                        listOfCosts.add(costOfDelivery);
-
-                    }catch (ParseException pe){
-                        System.out.print("parseException");
-                    }
-
-                }
-            }
-            String stringPrice = link.text().split(" z")[0];
-
-            System.out.println(stringPrice);
-
-            try {
-                NumberFormat format = NumberFormat.getInstance(Locale.FRANCE);
-                Number number = format.parse(stringPrice);
-                price = number.doubleValue();
-
-            }catch (ParseException pe){
-                System.out.print("parseException");
-            }
-
-
-            String category = link1.text();
-
-            System.out.println("Cena podstawowa: "+price+ " zł. " +"kategoria: "+
-                    category + " " +"koszty dostawy:" + listOfCosts.toString());
+            /*
+             * It's hard to understand here whether you pull documents from allegro or take it from disk
+             */
+            Document doc = Jsoup.parse(input,
+                    "UTF-8","http://allegro.pl/dobre-struny-do-gitary-akustycznej-10-47-3x-kostka-i4639566521.html");
+            return doc;
 
         } catch (IOException e) {
-            System.out.println("IOException");
-
+            /*
+            We need to handle it a bit better. For now I just leave the println and return null.
+             */
+            e.printStackTrace();
+            return null;
         }
 
 
 
     }
+
+    /*
+     * This method parses JSOUP document so we can deal with documents regardless of how they were created
+     * (i.e. by pulling website through http or using input file). Doing it this way let us reuse this code and
+     * makes testing way easier.
+     *
+     * For now this returns just price. We should create a class describing product so we can return an instance of it.
+     *
+     */
+    public double parseDocument(Document doc) {
+        double price = -1;
+        List<Double> listOfCosts = new LinkedList<Double>();
+        String costOfDeliveryString;
+        Element link = doc.select("strong[data-price]").first();
+        Element link1 = doc.select("span[itemprop]").first();
+        Elements link2 = doc.select("strong[class]");
+               /*Throw useless comments away before commit. (Do the same with my comments as well) */
+//            System.out.print(link2);
+//            System.out.println(link2.attr("class"));
+
+        for(Element src:link2){
+            if(src.attr("class").equals("whiteBg")){
+                costOfDeliveryString = src.text().trim().split(" z")[0];
+                try {
+                    NumberFormat format = NumberFormat.getInstance(Locale.FRANCE);
+                    Number number = format.parse(costOfDeliveryString);
+                    double costOfDelivery = number.doubleValue();
+                    listOfCosts.add(costOfDelivery);
+
+                }catch (ParseException pe){
+                    System.out.print("parseException");
+                }
+
+            }
+        }
+        String stringPrice = link.text().split(" z")[0];
+
+        System.out.println(stringPrice);
+
+
+        try {
+            NumberFormat format = NumberFormat.getInstance(Locale.FRANCE);
+            Number number = format.parse(stringPrice);
+            price = number.doubleValue();
+
+        }catch (ParseException pe){
+            pe.printStackTrace();
+        }
+
+
+        String category = link1.text();
+
+        System.out.println("Cena podstawowa: "+price+ " zł. " +"kategoria: "+
+                category + " " +"koszty dostawy:" + listOfCosts.toString());
+        return price;
+    }
+
 
 }
---
+++ src/test/java/com/db/ParserTest.java
@@ -0,0 +1,21 @@
+package com.db;
+
+import org.jsoup.nodes.Document;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+
+public class ParserTest {
+
+    @Test
+    public void testParseFromFile() throws Exception {
+        Parser p = new Parser();
+        Document doc = p.getJsoupDocument();
+        double price = p.parseDocument(doc);
+
+        assertEquals(9.9,price);
+
+
+    }
+}
